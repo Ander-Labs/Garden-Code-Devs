c97e5f99a7eb30839f96b40e1dead44fa1883fac
andersonruizr2 <andersonruizr2@gmail.com>
2024-09-02 16:50:31 +0000

    update

--- src/components/UI/resource/Sidemap.tsx
+++ src/components/UI/resource/Sidemap.tsx
@@ -23,16 +23,20 @@ export default function Sidemap({ children }: { children: React.ReactNode }) {
           <div className="mt-auto p-4">
             <Card x-chunk="dashboard-02-chunk-0">
               <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
+                <CardTitle>Patrocinar Garden Code</CardTitle>
                 <CardDescription>
                   Unlock all features and get unlimited access to our support
                   team.
                 </CardDescription>
               </CardHeader>
               <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Button size="sm" className="w-full">
-                  Upgrade
-                </Button>
+                <iframe
+                  src="https://github.com/sponsors/Ander-Labs/button"
+                  title="Sponsor Ander-Labs"
+                  height="32"
+                  width="114"
+                  className="border: 0; border-radius: 6px;"
+                ></iframe>
               </CardContent>
             </Card>
           </div>
